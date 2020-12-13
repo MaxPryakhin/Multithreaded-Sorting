051d5b71fba15d70bcfc6a2fb3bb14e7da8c4acf
Max Pryakhin <KlaronFirefly@gmail.com>
2020-12-13 17:07:07 +0300

    Create QuickSortStrategy.cs

---
+++ Multithreaded-Sorting/Multithreaded-Sorting/Sorter/QuickSortStrategy.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Multithreaded_Sorting.Sorter
+{
+    class QuickSortStrategy : ISortStrategy
+    {
+        public IEnumerable<int> SortChunk(IEnumerable<int> chunk) 
+        {
+            var list = chunk.ToList();
+            list.Sort();
+            return list;
+        }
+    }
+}
