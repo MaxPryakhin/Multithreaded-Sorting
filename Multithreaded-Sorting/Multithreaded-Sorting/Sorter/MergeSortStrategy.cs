572e86079858df67844b59b41bbc5f0d49b2be7b
Max Pryakhin <KlaronFirefly@gmail.com>
2020-12-13 17:13:06 +0300

    Create MergeSortStrategy.cs

---
+++ Multithreaded-Sorting/Multithreaded-Sorting/Sorter/MergeSortStrategy.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Multithreaded_Sorting.Sorter
+{
+    class MergeSortStrategy : ISortStrategy
+    {
+		int[] MergeSort(int[] array)
+		{
+			if (array.Length == 1)
+			{
+				return array;
+			}
+
+			int middle = array.Length / 2;
+			return Merge(MergeSort(array.Take(middle).ToArray()), MergeSort(array.Skip(middle).ToArray()));
+		}
+
+		int[] Merge(int[] arr1, int[] arr2)
+		{
+			int ptr1 = 0, ptr2 = 0;
+			int[] merged = new int[arr1.Length + arr2.Length];
+
+			for (int i = 0; i < merged.Length; ++i)
+			{
+				if (ptr1 < arr1.Length && ptr2 < arr2.Length)
+				{
+					merged[i] = arr1[ptr1] > arr2[ptr2] ? arr2[ptr2++] : arr1[ptr1++];
+				}
+				else
+				{
+					merged[i] = ptr2 < arr2.Length ? arr2[ptr2++] : arr1[ptr1++];
+				}
+			}
+
+			return merged;
+		}
+		public IEnumerable<int> SortChunk(IEnumerable<int> chunk)
+        {
+            return MergeSort(chunk.ToArray());
+        }
+    }
+}
