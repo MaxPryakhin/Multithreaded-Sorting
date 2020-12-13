6e6ee3465f16321bf6002b9a219c73f81b8cc7df
Max Pryakhin <KlaronFirefly@gmail.com>
2020-12-13 13:59:58 +0300

    init project

---
+++ Multithreaded-Sorting/Multithreaded-Sorting/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Multithreaded_Sorting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
